c2d496d1620116787bc6b7592c8b1242cdb23b3a
Mahima Kumari <mahimayadowansi@gmail.com>
2025-03-01 13:55:42 +0530

    Profile Element Navigation Tested

--- src/main/java/pages/ProfilePage.java
+++ src/main/java/pages/ProfilePage.java
@@ -1,5 +1,95 @@
 package pages;
 
-public class ProfilePage {
+import java.time.Duration;
+import java.util.Iterator;
+import java.util.Set;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ProfilePage extends BasePage {
+
+	public ProfilePage(WebDriver driver) {
+		super(driver);
+		PageFactory.initElements(driver, this);
+	}
+
+	// Locators
+	@FindBy(xpath = "//div[text()='Jobs']")
+	private WebElement jobPage;
+	@FindBy(xpath = "//div[text()='Companies']")
+	private WebElement companyPage;
+	@FindBy(xpath = "//div[text()='Services']")
+	private WebElement servicePage;
+	@FindBy(xpath = "//img[@alt='naukri360-pill']")
+	private WebElement naukri360Page;
+	@FindBy(xpath = "//span[contains(@class, 'ni-gnb-icn-bell')]")
+	private WebElement notificationPage;
+	
+	@FindBy(xpath="//a[@class='close']")
+	private WebElement closeNotification;
+	
+	@FindBy(xpath="//img[@alt='Naukri Logo']")
+	private WebElement logoClick;
+	
+	@FindBy(xpath = "//a[normalize-space()='View profile']")
+	private WebElement viewProfilePage;
+	@FindBy(xpath = "//div[contains(@class ,'nI-gNb-drawer__bars')]")
+	private WebElement drawerBarLinePage;
+
+	WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+public void testNavigation()
+{    wait.until(ExpectedConditions.visibilityOf(jobPage));
+	clickElement(jobPage);
+	System.out.println("The job page navigated");
+	wait.until(ExpectedConditions.visibilityOf(companyPage));
+	clickElement(companyPage);
+	System.out.println("The companyPage navigated");
+	wait.until(ExpectedConditions.visibilityOf(naukri360Page));
+	clickElement(naukri360Page);
+	System.out.println("The naukri360Page navigated");
+	
+	wait.until(ExpectedConditions.visibilityOf(notificationPage));
+	clickElement(notificationPage);
+	System.out.println("The notificationPage  navigated");
+	wait.until(ExpectedConditions.visibilityOf(closeNotification));
+	clickElement(closeNotification);
+	System.out.println("The closeNotification page navigated");
+	
+	
+	wait.until(ExpectedConditions.visibilityOf(logoClick));
+	clickElement(logoClick);
+	System.out.println("The logo homepage navigated");
+	wait.until(ExpectedConditions.visibilityOf(viewProfilePage));
+	clickElement(viewProfilePage);
+	System.out.println("The viewProfilePage navigated");
+	String ParentWindow = driver.getWindowHandle();
+
+	
+	wait.until(ExpectedConditions.visibilityOf(servicePage));
+	clickElement(servicePage);
+	
+	Set<String> windowsOpened = driver.getWindowHandles();
+	Iterator<String> itr = windowsOpened.iterator();
+
+	while (itr.hasNext()) {
+		String jobApplyWindow = itr.next();
+		if (!ParentWindow.equals(jobApplyWindow)) {
+			driver.switchTo().window(jobApplyWindow);
+			System.out.println(driver.getTitle());
+	
+	
 }
+
+		
+	}
+	driver.switchTo().defaultContent();
+	
+}
+}
+
--- src/test/java/testCases/ProfileTest.java
+++ src/test/java/testCases/ProfileTest.java
@@ -1,5 +1,27 @@
 package testCases;
 
-public class ProfileTest {
+import java.time.Duration;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
+import pages.ProfilePage;
+
+public class ProfileTest extends BaseTest {
+	
+	ProfilePage profile;
+	LoginPage login;
+	@Test
+	public void testNaukriNavigation() throws InterruptedException
+	{
+		
+		LoginTest loginTest = new LoginTest(); // Creating object of LoginTest
+		loginTest.driver = this.driver; // Assign the same driver instance
+		loginTest.login();
+		WebDriverWait wait=new WebDriverWait(driver, Duration.ofSeconds(5))	;
+		profile= new ProfilePage(driver);
+		profile.testNavigation();
+	}
 
 }
